c3bac2e02de3eb6f52c1dae02200f486c891cda5
Kevin Clark <k.j.clark126@gmail.com>
2020-07-21 06:45:38 +0000

    change formatting for ratcode notif

--- badrat_server.py
+++ badrat_server.py
@@ -67,7 +67,7 @@ def send_ratcode(ratID):
     else:
         # Careful for path traversal vuln... This code may not be 100 percent safe
         with open(os.getcwd() + "/rats/badrat." + types[ratID], 'r') as fd:
-            print("[*] sending ratcode to " + colors(ratID))
+            print("\n[*] sending ratcode to " + colors(ratID))
             return(fd.read())
 
 def serve_server(port=8080):
